c3f0a965b6bf9a97b189761b901d71b18d4cd29a
Yahav Bahat <yahavbahat@gmail.com>
2021-06-28 17:51:40 +0300

    Minor fixes.

--- Logging.py
+++ Logging.py
@@ -1,9 +1,25 @@
 import logging
 from rich.logging import RichHandler
+from rich.text import Text
 
 
 def log_setup(t):
     FORMAT = "%(message)s"
-    logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+    logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True)])
 
     return logging.getLogger(t)
+
+
+def get_underline(text):
+    text = Text(text)
+    text.stylize("underline")
+    return text
+
+
+def no_connection(text):
+    return f"Couldn't establish connection for [underline]{text}[/underline]\n"
+
+
+def successfully_authenticated(host, user, password):
+    return f"Successfully authenticated for [underline]{host}[/underline] with user: [underline]{user}[/underline] " \
+           f"and password: [underline]{password}[/underline]\n "
