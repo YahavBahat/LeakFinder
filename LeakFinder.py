af12c5fe06cbc04f821b4960a1f9ced7869dce43
Yahav Bahat <yahavbahat@gmail.com>
2021-06-28 17:46:44 +0300

    Minor fixes.

--- LeakFinder.py
+++ LeakFinder.py
@@ -52,18 +52,17 @@ def info_builder(host, port, cluster_obj, filter_obj, module_name):
 
 def main(host, port, patterns, match_against, size, output, format_, exclude_unmatched, include_geo, silent):
     cluster_obj, module_name = get_cluster_object(str(port))
-    try:
-        cluster_instance = cluster_obj(host, port)
+
+    cluster_instance = cluster_obj(host, port)
+    if not cluster_instance.error:
         cluster_method_manager(cluster_instance, patterns, match_against)
         filter_obj = Filter(cluster_instance, patterns, match_against, size)
         if not exclude_unmatched or any(
-            (filter_obj.pattern_match, filter_obj.size_match)
+                (filter_obj.pattern_match, filter_obj.size_match)
         ):
             Output(info_builder(host, port, cluster_instance, filter_obj, module_name), f"OUTPUT {filename}", output,
                    format_,
                    exclude_unmatched, include_geo, silent)
-    except Exception as e:
-        log.info(f"Couldn't establish connection for {host}\nError: {e}\n")
 
 
 @click.command()
