12c97bbee92ed32b3ff4efc16580b02a844f59f7
Yahav Bahat <yahavbahat@gmail.com>
2021-06-29 17:11:54 +0300

    Filter host with module using NMAP.

--- LeakFinder.py
+++ LeakFinder.py
@@ -6,16 +6,28 @@ from datetime import datetime
 from Logging import log_setup
 from concurrent.futures import ProcessPoolExecutor
 from Shodan import Shodan
+import nmap3
 
 log = log_setup("LeakFinder")
 cluster_ip = {"3306": "MySQL", "27017": "MongoDB", "9200": "ElasticSearch", "9042": "Cassandra"}
 filename = datetime.now().strftime("%m.%d.%Y %H:%M:%S")
+nmap = nmap3.Nmap()
 
 
-def get_cluster_object(port):
+def get_cluster_object(host, port, version_scan):
     module_name = cluster_ip.get(port)
     if not module_name:
-        log.error(f"Port {port} is not linked with any module. Available modules by host ports \n{cluster_ip}")
+        if version_scan:
+            log.warning(f"Port {port} is not linked with any module. Trying NMAP service scan...")
+            module_name = [module_name for module_name in cluster_ip.values() if
+                           module_name.lower() in get_service(host, port).lower()]
+            if module_name:
+                module_name = module_name[-1]
+            else:
+                log.warning(f"Port {port} is not linked with any module. Available modules by host ports \n{cluster_ip}"
+                            )
+        else:
+            log.warning(f"Port {port} is not linked with any module. Available modules by host ports \n{cluster_ip}")
     module = import_module(f"API.{module_name}")
     return getattr(module, module_name), module_name
 
@@ -56,9 +68,17 @@ def info_builder(host, port, cluster_obj, filter_obj, module_name, shodan):
     return info
 
 
+def get_service(host, port):
+    try:
+        results = nmap.scan_top_ports(host, args=f"-sV -Pn -p {port}")
+        return results.get(host).get("ports")[0].get("service").get("product")
+    except Exception:
+        pass
+
+
 def main(host, port, patterns, match_against, size, output, format_, exclude_unmatched, include_geo, try_default,
-         shodan, silent):
-    cluster_obj, module_name = get_cluster_object(str(port))
+         shodan, version_scan, silent):
+    cluster_obj, module_name = get_cluster_object(host, str(port), version_scan)
     cluster_instance = cluster_obj(host, port, try_default)
     if not cluster_instance.error:
         cluster_method_manager(cluster_instance, patterns, match_against)
@@ -73,6 +93,14 @@ def main(host, port, patterns, match_against, size, output, format_, exclude_unm
 @click.command()
 @click.option("--hosts-file", "-h", help="Path to filename with hosts [IP:PORT] format, separated by a newline.",
               required=True)
+@click.option("--version-scan", "-v", is_flag=True, help="The program filters hosts to their suitable modules "
+                                                         "(e.g., MongoDB, Cassandra, Elasticsearch) by comparing the  "
+                                                         "module/service's default port to the host's port."
+                                                         " However, sometimes there are hosts with a port different from"
+                                                         " the default one (e.g., port 9201 instead of 9200 for ES),"
+                                                         " and in those cases, the program can use NMAP service scan to"
+                                                         " find out the host's service product, thus matching it to its"
+                                                         " suitable module.")
 @click.option("--patterns", "-p", help="Filter clusters by regex patterns."
                                        " Path to regex patterns file, each pattern separated by a newline.")
 @click.option("--match-against", "-m", help="Where to match regex patterns.", type=click.Choice(
@@ -92,8 +120,8 @@ def main(host, port, patterns, match_against, size, output, format_, exclude_unm
 @click.option("--shodan", "-sn", help="To get vulnerabilities of matched clusters Shodan API key is required."
                                       " Either pass as a CLI argument or input the API key at config.config file.")
 @click.option("--silent", is_flag=True, help="No terminal output.")
-def wrapper(hosts_file, patterns, match_against, size, output, format_, exclude_unmatched, include_geo, processes,
-            try_default, shodan, silent):
+def wrapper(hosts_file, version_scan, patterns, match_against, size, output, format_, exclude_unmatched, include_geo,
+            processes, try_default, shodan, silent):
     valid_file(patterns, "patterns", log)
     valid_file(hosts_file, "hosts_file", log)
 
@@ -102,7 +130,7 @@ def wrapper(hosts_file, patterns, match_against, size, output, format_, exclude_
         line = line.strip().split(":")
         host, port = line[0], int(line[1])
         executor.submit(main, host, port, patterns, match_against, size, output, format_, exclude_unmatched,
-                        include_geo, try_default, shodan, silent)
+                        include_geo, try_default, shodan, version_scan, silent)
 
 
 # TODO: add an option to parse hosts from other formats, (CSV, JSON)
